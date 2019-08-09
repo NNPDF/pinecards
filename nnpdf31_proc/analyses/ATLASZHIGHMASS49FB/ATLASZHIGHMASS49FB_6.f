b9f91ffb6e79d30de7a8fdb8a3634830d9d520be
enocera <emanuele.nocera@ed.ac.uk>
2019-08-09 10:35:03 +0100

    Added ATLAS Z high mass DY

---
+++ nnpdf31_proc/analyses/ATLASZHIGHMASS49FB/ATLASZHIGHMASS49FB_6.f
@@ -0,0 +1,71 @@
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      subroutine analysis_begin(nwgt,weights_info)
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      implicit none
+      integer nwgt
+      character*(*) weights_info(*)
+
+      call HwU_inithist(nwgt,weights_info)
+      call HwU_book(1,'lmlp inv m', 1, 210d0, 230d0)
+      return
+      end
+
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      subroutine analysis_end(dummy)
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      implicit none
+      double precision dummy
+      call HwU_write_file
+      return                
+      end
+
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      subroutine analysis_fill(p,istatus,ipdg,wgts,ibody)
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      implicit none
+      include 'nexternal.inc'
+      integer istatus(nexternal)
+      integer iPDG(nexternal)
+      integer ibody  
+      integer i
+      double precision p(0:4,nexternal)
+      double precision wgts(*)
+      double precision ppl(0:3), pplb(0:3), ppv(0:3), xmll, getinvm
+      external getinvm
+
+      do i=0,3
+        ppl(i)=p(i,4)
+        pplb(i)=p(i,3)
+        ppv(i)=ppl(i)+pplb(i)
+      enddo
+
+      xmll=getinvm(ppv(0),ppv(1),ppv(2),ppv(3))
+
+      call HwU_fill(1,xmll,wgts)
+
+ 999  return      
+      end
+
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      function getinvm(en,ptx,pty,pl)
+      implicit none
+      real*8 getinvm,en,ptx,pty,pl,tiny,tmp
+      parameter (tiny=1.d-5)
+c
+      tmp=en**2-ptx**2-pty**2-pl**2
+      if(tmp.gt.0.d0)then
+        tmp=sqrt(tmp)
+      elseif(tmp.gt.-tiny)then
+        tmp=0.d0
+      else
+        write(*,*)'Attempt to compute a negative mass'
+        stop
+      endif
+      getinvm=tmp
+      return
+      end
+

e8359814677b9b2f81ef6751b556591ab28616ec
Christopher Schwan <handgranaten-herbert@posteo.de>
2019-10-28 17:48:29 +0100

    Remove `getTH3D` function

--- applgridphoton/src/sparse_matrix3d_root_interface.cpp
+++ applgridphoton/src/sparse_matrix3d_root_interface.cpp
@@ -1,6 +1,8 @@
 #include "sparse_matrix3d_root_interface.hpp"
 
 #include "tsparse2d.h"
+#include "tsparse1d.h"
+
 #include <TH1D.h>
 
 SparseMatrix3d* sparse_matrix3d_from_th3d(TH3D const& histogram)
@@ -32,4 +34,52 @@ SparseMatrix3d* sparse_matrix3d_from_th3d(TH3D const& histogram)
     }
 
     return matrix;
+}
+
+TH3D* sparse_matrix3d_to_th3d(SparseMatrix3d const& matrix, std::string const& s)
+{
+    double const delx = matrix.xaxis().delta();
+    double const dely = matrix.yaxis().delta();
+    double const delz = matrix.zaxis().delta();
+
+    TH3D* h = new TH3D(
+        s.c_str(),
+        s.c_str(),
+        matrix.xaxis().N(),
+        matrix.xaxis().min()-0.5*delx,
+        matrix.xaxis().max()+0.5*delx,
+        matrix.yaxis().N(),
+        matrix.yaxis().min()-0.5*dely,
+        matrix.yaxis().max()+0.5*dely,
+        matrix.zaxis().N(),
+        matrix.zaxis().min()-0.5*delz,
+        matrix.zaxis().max()+0.5*delz
+    );
+
+    for (std::size_t i = matrix.lo(); i <= matrix.hi(); ++i)
+    {
+        tsparse2d<double> const* s2d = matrix[i];
+
+        if (s2d == nullptr)
+        {
+            continue;
+        }
+
+        for (std::size_t j = s2d->lo(); j <= s2d->hi(); ++j)
+        {
+            tsparse1d<double> const* s1d = (*s2d)[j];
+
+            if (s1d == nullptr)
+            {
+                continue;
+            }
+
+            for (std::size_t k = s1d->lo(); k <= s1d->hi(); ++k)
+            {
+                h->SetBinContent(i + 1, j + 1, k + 1, (*s1d)(k));
+            }
+        }
+    }
+
+    return h;
 }

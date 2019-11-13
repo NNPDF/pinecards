61f43381c13b3f58bb6b7e8d978e98eac83fc985
Christopher Schwan <handgranaten-herbert@posteo.de>
2019-11-13 16:33:09 +0100

    Add missing return statement

--- applgridphoton/src/merge_bins.cpp
+++ applgridphoton/src/merge_bins.cpp
@@ -11,6 +11,8 @@ int main(int argc, char* argv[])
     if (argc < 3)
     {
         std::cerr << "Usage: merge_bins outgrid grid1 [grid2 ...]\n";
+
+        return 1;
     }
 
     std::vector<std::string> args(&argv[2], &argv[argc]);

67654b986d90a6929f67e2327e0c50c63f244da7
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-01-07 17:39:45 +0100

    Improve details of run.sh

--- run.sh
+++ run.sh
@@ -6,32 +6,36 @@ set -o errexit
 # the following exits if undeclared variables are used
 set -o nounset
 
-if [[ $# != 1 ]] ; then
-    echo "Usage: ./run.sh [nameset]"
-    echo "  The following namesets are available:"
+# check arguments of the script
+if (( $# != 1 )); then
+    echo "Usage: ./run.sh [nameset]" >&2
+    echo "  The following namesets are available:" >&2
 
     for i in $(find nnpdf31_proc/analyses -name *.f); do
         name=${i##*/}
         nameset=${name%%.f}
-        echo "  - ${nameset}"
+        echo "  - ${nameset}" >&2
     done
 
     exit 2
 fi
 
+# name of the experiment
 experiment="$1"
+
+# name of the directory where the output is written to
 output="$experiment"-$(date +%Y%m%d%H%M%S)
 
-if [ -d "$output" ]; then
+if [[ -d $output ]]; then
     # since we add a date postfix to the name this shouldn't happen
-    echo "Error: output directory already exists"
+    echo "Error: output directory already exists" >&2
     exit 2
 fi
 
 mg5amc=$(which mg5_aMC)
 
-if [[ ! -x "${mg5amc}" ]]; then
-    echo "The binary \`mg5_aMC\' wasn't found. Please adjust your PATH variable."
+if [[ ! -x ${mg5amc} ]]; then
+    echo "The binary \`mg5_aMC\' wasn't found. Please adjust your PATH variable" >&2
     exit 1
 fi
 
@@ -39,6 +43,7 @@ fi
 tmpdir=$(mktemp -d)
 trap 'rm -rf "$tmpdir"' EXIT
 
+# copy the output file to the temporary directory and replace the variables
 mkdir "$tmpdir"/output
 output_file="$tmpdir"/output/$experiment.txt
 cp nnpdf31_proc/output/$experiment.txt "$output_file"
@@ -49,15 +54,14 @@ python2 "${mg5amc}" "$output_file"
 
 # copy patches if there are any
 if [[ -d nnpdf31_proc/patches/$experiment ]]; then
-    echo "Copying patches"
     cp -r nnpdf31_proc/patches/$experiment/* "$output"/
 fi
 
 # enforce proper analysis
-echo "Copying the relevant analysis"
 cp nnpdf31_proc/analyses/$experiment.f "$output"/FixedOrderAnalysis
 sed -i "s/analysis_HwU_template/$experiment/g" "$output"/Cards/FO_analyse_card.dat
 
+# copy the launch file to the temporary directory and replace the variables
 mkdir "$tmpdir"/launch
 launch_file="$tmpdir"/launch/$experiment.txt
 cp nnpdf31_proc/launch/$experiment.txt "$launch_file"
@@ -75,5 +79,4 @@ EOF
 sed -f <(sed -E 's|(.*) (.*)|s/@\1@/\2/|g' "$tmpdir"/variables.txt) -i "$launch_file"
 
 # launch run
-echo "Launch mg5_aMC"
 python2 "${mg5amc}" "$launch_file"

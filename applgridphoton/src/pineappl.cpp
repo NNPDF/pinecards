NNPDF/pinecards

commit 116f5dcd922dffd7d394ad5d200de6c9e249be66

Use pointers instead of indices for grid functions

applgridphoton/src/pineappl.cpp
@@ -7,18 +7,8 @@
 #include <array>
 #include <cassert>
 #include <string>
+#include <utility>
 #include <vector>
-
-namespace
-{
-
-std::vector<appl::grid>& appl_grids()
-{
-    static std::vector<appl::grid> appl_grids_;
-    return appl_grids_;
-}
-
-}
 
 struct pineappl_lumi
 {
@@ -55,7 +45,17 @@ void pineappl_lumi_add(pineappl_lumi* lumi, unsigned combinations, int* pdg_id_p
     lumi->combinations.emplace_back(std::vector<int>(pdg_id_pairs, pdg_id_pairs + 2 * combinations));
 }
 
-int pineappl_grid_new(
+struct pineappl_grid
+{
+    pineappl_grid(appl::grid&& grid)
+        : grid(std::move(grid))
+    {
+    }
+
+    appl::grid grid;
+};
+
+pineappl_grid* pineappl_grid_new(
     int n_bins,
     double const* bin_limits,
     pineappl_lumi* lumi,
@@ -72,8 +72,6 @@ int pineappl_grid_new(
     unsigned x_order,
     char const* map
 ) {
-    int const index = appl_grids().size();
-
     // STEP 1: prepare the vector containing the grid parameters for all grids
 
     // TODO: other format are currently not implemented
@@ -93,7 +91,7 @@ int pineappl_grid_new(
 
     // STEP 2: prepare the PDF combinations
 
-    std::string const pdf_name = "pineappl_appl_grid_pdf_bridge_" + std::to_string(index);
+    std::string const pdf_name = "pineappl_appl_grid_pdf_bridge_0"; // TODO: add unique identifier
     std::vector<int> lumi_vector;
     lumi_vector.push_back(lumi->combinations.size());
 
@@ -114,19 +112,17 @@ int pineappl_grid_new(
 
     // STEP 3: create the appl grids
 
-    appl_grids().emplace_back(n_bins, bin_limits, nq2, q2_min, q2_max, q2_order, nx, x_min, x_max,
-        x_order, pdf_name, order_ids, map);
-
-    return index;
+    return new pineappl_grid(appl::grid(n_bins, bin_limits, nq2, q2_min, q2_max, q2_order, nx,
+        x_min, x_max, x_order, pdf_name, order_ids, map));
 }
 
-void pineappl_grid_delete(int /*grid_id*/)
+void pineappl_grid_delete(pineappl_grid* grid)
 {
-    // TODO: deallocate memory
+    delete grid;
 }
 
 void pineappl_grid_fill(
-    int grid_id,
+    pineappl_grid* grid,
     double x1,
     double x2,
     double q2,
@@ -134,21 +130,21 @@ void pineappl_grid_fill(
     double const* weight,
     unsigned grid_index
 ) {
-    appl_grids().at(grid_id).fill_grid(x1, x2, q2, observable, weight, grid_index);
+    grid->grid.fill_grid(x1, x2, q2, observable, weight, grid_index);
 }
 
-void pineappl_grid_scale(int grid_id, double factor)
+void pineappl_grid_scale(pineappl_grid* grid, double factor)
 {
-    appl_grids().at(grid_id) *= factor;
+    grid->grid *= factor;
 }
 
-void pineappl_grid_write(int grid_id, char const* filename)
+void pineappl_grid_write(pineappl_grid* grid, char const* filename)
 {
-    appl_grids().at(grid_id).Write(filename);
+    grid->grid.Write(filename);
 }
 
 void pineappl_grid_convolute(
-    int grid_id,
+    pineappl_grid* grid,
     pineappl_func_xfx pdf1,
     pineappl_func_xfx pdf2,
     pineappl_func_alphas alphas,
@@ -161,7 +157,7 @@ void pineappl_grid_convolute(
     // TODO: selecting and deselecting grids is not completely possible with APPLgrid
     assert( grid_mask == nullptr );
 
-    auto const result = appl_grids().at(grid_id).vconvolute(pdf1, pdf2, alphas, 99, scale_ren,
+    auto const result = grid->grid.vconvolute(pdf1, pdf2, alphas, 99, scale_ren,
         scale_fac, scale_energy);
 
     std::copy(result.begin(), result.end(), bins);

applgridphoton/include/pineappl.h
@@ -47,7 +47,10 @@ typedef enum
 pineappl_grid_format;
 
 ///
-int pineappl_grid_new(
+struct pineappl_grid;
+
+///
+pineappl_grid* pineappl_grid_new(
     int n_bins,
     double const* bin_limits,
     pineappl_lumi* lumi,
@@ -66,11 +69,11 @@ int pineappl_grid_new(
 );
 
 ///
-void pineappl_grid_delete(int grid_id);
+void pineappl_grid_delete(pineappl_grid* grid);
 
 ///
 void pineappl_grid_fill(
-    int grid_id,
+    pineappl_grid* grid,
     double x1,
     double x2,
     double q2,
@@ -80,10 +83,10 @@ void pineappl_grid_fill(
 );
 
 ///
-void pineappl_grid_scale(int grid_id, double factor);
+void pineappl_grid_scale(pineappl_grid* grid, double factor);
 
 ///
-void pineappl_grid_write(int grid_id, char const* filename);
+void pineappl_grid_write(pineappl_grid* grid, char const* filename);
 
 // TODO: remove the `const&`
 
@@ -95,7 +98,7 @@ typedef double (*pineappl_func_alphas)(double const& q2);
 
 ///
 void pineappl_grid_convolute(
-    int grid_id,
+    pineappl_grid* grid,
     pineappl_func_xfx pdf1,
     pineappl_func_xfx pdf2,
     pineappl_func_alphas alphas,

applgridphoton/tests/basic.cpp
@@ -40,7 +40,7 @@ TEST_CASE("", "")
     std::array<double, 2> bin_limits = { 0.0, 1.0 };
 
     // create a new file
-    int grid_id = pineappl_grid_new(
+    auto* grid = pineappl_grid_new(
         bin_limits.size() - 1,
         bin_limits.data(),
         lumi,
@@ -61,17 +61,14 @@ TEST_CASE("", "")
     // delete luminosity function
     pineappl_lumi_delete(lumi);
 
-    // this is the first time that we call the function, thus the index must be zero
-    CHECK( grid_id == 0 );
-
     std::array<double, 1> const weights = { 1.0 };
 
-    pineappl_grid_fill(grid_id, 0.25, 0.25, 10000.0, 0.25, weights.data(), 0);
+    pineappl_grid_fill(grid, 0.25, 0.25, 10000.0, 0.25, weights.data(), 0);
 
     double result;
 
     pineappl_grid_convolute(
-        grid_id,
+        grid,
         simple_pdf,
         simple_pdf,
         alphas,
@@ -83,4 +80,6 @@ TEST_CASE("", "")
     );
 
     CHECK( result == 1.0 );
+
+    pineappl_grid_delete(grid);
 }

commit 4c9e4fe59a89c03947fecdd946fb8435487fa516

Fix bug introduced with commit 116f5dc

applgridphoton/src/pineappl.cpp
@@ -6,9 +6,27 @@
 #include <algorithm>
 #include <array>
 #include <cassert>
+#include <chrono>
 #include <string>
 #include <utility>
 #include <vector>
+
+namespace
+{
+
+std::string get_unique_id()
+{
+    using std::chrono::duration_cast;
+    using std::chrono::high_resolution_clock;
+    using std::chrono::nanoseconds;
+
+    auto const duration = high_resolution_clock::now().time_since_epoch();
+    auto const count = duration_cast<nanoseconds>(duration).count();
+
+    return std::to_string(count);
+}
+
+}
 
 struct pineappl_lumi
 {
@@ -95,7 +113,7 @@ pineappl_grid* pineappl_grid_new(
 
     // STEP 2: prepare the PDF pdg id pairs
 
-    std::string const pdf_name = "pineappl_appl_grid_pdf_bridge_0"; // TODO: add unique identifier
+    std::string const pdf_name = "pineappl_appl_grid_pdf_bridge_" + get_unique_id();
     std::vector<int> lumi_vector;
     lumi_vector.push_back(lumi->pdg_ids.size());
 
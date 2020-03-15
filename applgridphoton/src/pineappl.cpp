f9bf1e321cda1e7ccd0a3534e067c1779fe6f0e7
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-03-15 12:22:49 +0100

    Support `factors == nullptr`
    - if factors is set to a null pointer, this is treated as factors set to
      one for each combination

--- applgridphoton/src/pineappl.cpp
+++ applgridphoton/src/pineappl.cpp
@@ -83,9 +83,18 @@ void pineappl_lumi_add(
     }
 
     // TODO: non-unity factors are NYI
-    assert( std::all_of(factors, factors + combinations, [](double f) { return f == 1.0; }) );
+    assert( (factors == nullptr) ||
+        std::all_of(factors, factors + combinations, [](double f) { return f == 1.0; }) );
 
-    lumi->factors.emplace_back(factors, factors + combinations);
+    if (factors == nullptr)
+    {
+        lumi->factors.emplace_back(combinations, 1.0);
+    }
+    else
+    {
+        lumi->factors.emplace_back(factors, factors + combinations);
+    }
+
     lumi->pdg_ids.emplace_back(pdg_id_pairs, pdg_id_pairs + 2 * combinations);
 }
 

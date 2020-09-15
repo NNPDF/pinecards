0080c72867a82acaf9c299fe6642b1a99d4499d9
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-09-15 20:33:43 +0200

    Add CMSWMU8TEV_WM dataset

---
+++ nnpdf31_proc/CMSWMU8TEV_WP/analysis.f
@@ -0,0 +1,80 @@
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      subroutine analysis_begin(nwgt,weights_info)
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      implicit none
+      integer nwgt
+      character*(*) weights_info(*)
+
+      call set_error_estimation(1)
+      call HwU_inithist(nwgt,weights_info)
+      call HwU_book(1,'rap', 8, 0.0d0, 1.6d0)
+      call HwU_book(2,'rap', 2, 1.6d0, 2.1d0)
+      call HwU_book(3,'rap', 1, 2.1d0, 2.4d0)
+      return
+      end
+
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      subroutine analysis_end(dummy)
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      implicit none
+      double precision dummy
+      call HwU_write_file
+      return
+      end
+
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      subroutine analysis_fill(p,istatus,ipdg,wgts,ibody)
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      implicit none
+      include 'nexternal.inc'
+      include 'cuts.inc'
+      integer istatus(nexternal)
+      integer iPDG(nexternal)
+      integer ibody  
+      integer i
+      integer j
+      double precision p(0:4,nexternal)
+      double precision wgts(*)
+      double precision ppl(0:3), pplb(0:3), ppv(0:3), xyll, getabsy
+      external getabsy
+
+      double precision p_reco(0:4,nexternal)
+      integer iPDG_reco(nexternal)
+
+
+
+      call recombine_momenta(rphreco, etaphreco, lepphreco, quarkphreco,
+     $                       p, iPDG, p_reco, iPDG_reco)
+
+      do j = nincoming+1, nexternal
+        if (iPDG_reco(j).eq.-13) pplb(0:3)=p_reco(0:3,j)
+      enddo
+
+      xyll=getabsy(pplb(0),pplb(3))
+
+      call HwU_fill(1,xyll,wgts)
+      call HwU_fill(2,xyll,wgts)
+      call HwU_fill(3,xyll,wgts)
+
+ 999  return
+      end
+
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      function getabsy(en,pl)
+      implicit none
+      real*8 getabsy,en,pl,tmp
+c
+      tmp=pl/en
+      if(abs(tmp).lt.1d0)then
+        tmp=abs(atanh(tmp))
+      else
+        write(*,*)'Attempt to compute atanh(x) with x > 1'
+        stop
+      endif
+      getabsy=tmp
+      return
+      end

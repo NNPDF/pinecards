a8d63a773f2e82613b8900eac024785a0d93ffe7
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-12-17 10:22:54 +0100

    Add cms_2jet_3d_8tev cut

--- run_implement_user_defined_cuts.py
+++ run_implement_user_defined_cuts.py
@@ -9,6 +9,9 @@ cuts_variables = {
       real*8 zpzll,zmll,zp1p,zp1m,zp2p,zp2m,zpt2ll
       integer zlep,zalep
 ''',
+    'cms_2jet_3d_8tev': '''c
+      real*8 xystar,xyboost,xptavg
+''',
 }
 
 cuts_code = {
@@ -202,6 +205,76 @@ c             implementation of first formula on page 6 of https://arxiv.org/abs
           enddo
         endif
       enddo
+
+''',
+    'cms_2jet_3d_8tev': '''c
+      if ({}) then
+        if (njet.lt.2) then
+          passcuts_user=.false.
+          return
+        endif
+
+        xystar = 0.5d0 * dabs(atanh(pjet(3,1)/pjet(0,1))-
+     $                        atanh(pjet(3,2)/pjet(0,2)))
+        xyboost = 0.5d0 * dabs(atanh(pjet(3,1)/pjet(0,1))+
+     $                        atanh(pjet(3,2)/pjet(0,2)))
+        xptavg = 0.5d0 * (pt_04(pjet(0,1)) + pt_04(pjet(0,2)))
+
+        if (xptavg.lt.133d0) then
+          passcuts_user=.false.
+          return
+        endif
+
+        if (xyboost.lt.1d0) then
+          if (xystar.lt.1d0) then
+            if (xptavg.gt.1784d0) then
+              passcuts_user=.false.
+              return
+            endif
+          elseif (xystar.lt.2d0) then
+            if (xptavg.gt.1248d0) then
+              passcuts_user=.false.
+              return
+            endif
+          elseif (xystar.lt.3d0) then
+            if (xptavg.gt.548d0) then
+              passcuts_user=.false.
+              return
+            endif
+          else
+            passcuts_user=.false.
+            return
+          endif
+        else if (xystar.lt.2d0) then
+          if (xystar.lt.1d0) then
+            if (xptavg.gt.1032d0) then
+              passcuts_user=.false.
+              return
+            endif
+          elseif (xystar.lt.2d0) then
+            if (xptavg.gt.686d0) then
+              passcuts_user=.false.
+              return
+            endif
+          else
+            passcuts_user=.false.
+            return
+          endif
+        else if (xystar.lt.3d0) then
+          if (xystar.lt.1d0) then
+            if (xptavg.gt.430d0) then
+              passcuts_user=.false.
+              return
+            endif
+          else
+            passcuts_user=.false.
+            return
+          endif
+        else
+          passcuts_user=.false.
+          return
+        endif
+      endif
 
 ''',
 }

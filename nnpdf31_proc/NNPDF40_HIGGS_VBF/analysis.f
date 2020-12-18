fdbea708c76325c39b7473335ac6c5e92abb29b8
Christopher Schwan <handgranaten-herbert@posteo.de>
2020-12-18 23:24:07 +0100

    Move two cuts into the analysis file

--- nnpdf31_proc/NNPDF40_HIGGS_VBF/analysis.f
+++ nnpdf31_proc/NNPDF40_HIGGS_VBF/analysis.f
@@ -35,9 +35,105 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
       double precision p(0:4,nexternal)
       double precision wgts(*)
 
-      call HwU_fill(1,0.5d0,wgts)
+c     variables for amcatnlo_fastjetppgenkt
+      double precision pQCD(0:3,nexternal),rfj,sycut,palg
+     $     ,pjet(0:3,nexternal),etajet(nexternal)
+      integer nQCD,jet(nexternal),njet
+
+c     observables
+      double precision xmjj,yjet(nexternal)
+
+c     functions
+      double precision getinvm,getrapidityv4
+      external getinvm,getrapidityv4
+
+c     miscellaneous
+      integer i,j
+
+      nQCD=0
+      do j=nincoming+1,nexternal
+         if (abs(ipdg(j)).le.5 .or. ipdg(j).eq.21 .or.
+     $                              ipdg(j).eq.22) then
+            nQCD=nQCD+1
+            do i=0,3
+               pQCD(i,nQCD)=p(i,j)
+            enddo
+         endif
+      enddo
+
+      do i=1,nexternal
+         do j=0,3
+            pjet(j,i)=0d0
+         enddo
+         jet(i)=0
+      enddo
+
+c     recombine momenta
+      call amcatnlo_fastjetppgenkt_etamax(pQCD,nQCD,jetradius,ptj,etaj
+     $     ,jetalgo,pjet,njet,jet)
+
+      do i=1,njet
+        yjet(i)=getrapidityv4(pjet(0,i))
+      enddo
+
+      xmjj = getinvm(pjet(0,1)+pjet(0,2),
+     $               pjet(1,1)+pjet(1,2),
+     $               pjet(2,1)+pjet(2,2),
+     $               pjet(3,1)+pjet(3,2))
+
+c     APPLY the proper invariant mass cut here to avoid empty channels
+      if (xmjj.gt.500d0 .and. abs(yjet(1)-yjet(2)).gt.2.5d0) then
+        call HwU_fill(1,0.5d0,wgts)
+      endif
 
  999  return
       end
 
 cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      function getinvm(en,ptx,pty,pl)
+      implicit none
+      real*8 getinvm,en,ptx,pty,pl,tiny,tmp
+      parameter (tiny=1.d-5)
+c
+      tmp=en**2-ptx**2-pty**2-pl**2
+      if(tmp.gt.0.d0)then
+        tmp=sqrt(tmp)
+      elseif(tmp.gt.-tiny)then
+        tmp=0.d0
+      else
+        write(*,*)'Attempt to compute a negative mass'
+        stop
+      endif
+      getinvm=tmp
+      return
+      end
+
+      function getrapidity(en,pl)
+      implicit none
+      real*8 getrapidity,en,pl,tiny,xplus,xminus,y
+      parameter (tiny=1.d-8)
+c
+      xplus=en+pl
+      xminus=en-pl
+      if(xplus.gt.tiny.and.xminus.gt.tiny)then
+        if( (xplus/xminus).gt.tiny.and.(xminus/xplus).gt.tiny )then
+          y=0.5d0*log( xplus/xminus )
+        else
+          y=sign(1.d0,pl)*1.d8
+        endif
+      else
+        y=sign(1.d0,pl)*1.d8
+      endif
+      getrapidity=y
+      return
+      end
+
+      function getrapidityv4(p)
+      implicit none
+      real*8 getrapidityv4,p(0:3)
+      real*8 getrapidity
+c
+      getrapidityv4=getrapidity(p(0),p(3))
+      return
+      end

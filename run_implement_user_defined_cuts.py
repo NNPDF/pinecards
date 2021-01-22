6cdf60aa17fe87b2784d1670bf8df8be50b4bc4a
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-01-22 13:51:03 +0100

    Fix cuts

--- run_implement_user_defined_cuts.py
+++ run_implement_user_defined_cuts.py
@@ -223,27 +223,27 @@ c             exit
             endif
           else if (xyj.lt.1.0d0) then
             if (xptj.le.2500d0) then
-              xjet=.false.
+              xjet=.true.
               exit
             endif
           else if (xyj.lt.1.5d0) then
             if (xptj.le.1992d0) then
-              xjet=.false.
+              xjet=.true.
               exit
             endif
           else if (xyj.lt.2.0d0) then
             if (xptj.le.1310d0) then
-              xjet=.false.
+              xjet=.true.
               exit
             endif
           else if (xyj.lt.2.5d0) then
             if (xptj.le.838d0) then
-              xjet=.false.
+              xjet=.true.
               exit
             endif
           else if (xyj.lt.3.0d0) then
             if (xptj.le.565d0) then
-              xjet=.false.
+              xjet=.true.
               exit
             endif
           endif

b3c43334ed6b5b8a4defbe45e2dab18283d24fe7
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-02-11 13:05:49 +0100

    Simplify `postrun.sh` files

--- nnpdf31_proc/LHCBZEE2FB/postrun.sh
+++ nnpdf31_proc/LHCBZEE2FB/postrun.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-NEW_GRID="${GRID%.lz4}"
-
-# divide the grid values by two
-pineappl merge "${NEW_GRID}" "${GRID}" --scale 0.5
-# compress the new grid
-lz4 -9 -f "${NEW_GRID}"
-# remove the uncompressed grid
-rm "${NEW_GRID}"
+pineappl merge "${GRID}".tmp "${GRID}" --scale 0.5
+mv "${GRID}".tmp "${GRID}"

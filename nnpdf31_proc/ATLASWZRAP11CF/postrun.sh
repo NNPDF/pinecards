0527fc64af3c22d85b26351463c8a18561400eef
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-02-17 15:32:14 +0100

    Remove superfluous normalization factor

--- nnpdf31_proc/ATLASWZRAP11CF/postrun.sh
+++ nnpdf31_proc/ATLASWZRAP11CF/postrun.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pineappl remap "${GRID}" "${GRID}".tmp '66,116,150;1.2,1.4,1.6,1.8,2,2.2,2.4,2.8,3.2,3.6|1.2,1.6,2,2.4,2.8,3.2,3.6' --ignore_obs_norm 1 --norm 2
+pineappl remap "${GRID}" "${GRID}".tmp '66,116,150;1.2,1.4,1.6,1.8,2,2.2,2.4,2.8,3.2,3.6|1.2,1.6,2,2.4,2.8,3.2,3.6' --ignore_obs_norm 1
 mv "${GRID}".tmp "${GRID}"

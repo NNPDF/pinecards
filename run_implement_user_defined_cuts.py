231a1ce84331fa5a14efdb80fd82dc7851ee21a7
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-02-24 22:18:35 +0100

    Optimize `mtw` cut

--- run_implement_user_defined_cuts.py
+++ run_implement_user_defined_cuts.py
@@ -394,7 +394,7 @@ c             implementation of first formula on page 6 of https://arxiv.org/abs
 ''',
     'mtw': '''c     cut on the transverse mass of W bosons
       do i=3,nexternal
-        do j=3,nexternal
+        do j=i+1,nexternal
           if (is_a_lm(i) .or. is_a_lp(i) .or.
      &        is_a_lm(j) .or. is_a_lp(j)) then
             if (abs(ipdg_reco(i)+ipdg_reco(j)).eq.1) then

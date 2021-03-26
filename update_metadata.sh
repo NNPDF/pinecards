442df65f989194b5dc3ad3190f957acd04897330
Christopher Schwan <handgranaten-herbert@posteo.de>
2021-03-26 08:43:26 +0100

    Improve script

--- update_metadata.sh
+++ update_metadata.sh
@@ -9,26 +9,23 @@ set -o nounset
 # exit if some program in a pipeline fails
 set -o pipefail
 
-for i in $(find -maxdepth 1 -mindepth 1 -type d); do
-    dataset=${i#./}
-    dataset=${dataset%-[0-9]*}
-    grid=$i/${dataset}.pineappl.lz4
+for i in $(find -name '*.pineappl.lz4'); do
+    dataset=$(basename $i)
+    dataset=${dataset%.pineappl.lz4}
 
-    if [[ -f ${grid} ]]; then
-        export grid
-        export metadata=nnpdf31_proc/${dataset}/metadata.txt
+    export grid=${i}
+    export metadata=nnpdf31_proc/${dataset}/metadata.txt
 
-        if [[ ! -f ${metadata} ]]; then
-            echo "Skipping ${grid#./}, couldn't find metadata (is the git repository on the right branch?)"
-            continue
-        else
-            echo "Updating ${grid#./}"
-        fi
-
-        eval $(awk -F= "BEGIN { printf \"pineappl set ${grid} ${grid}.tmp \" } { printf \"--entry %s '%s' \", \$1, \$2 }" ${metadata})
-
-        lz4 -9 ${i}/${dataset}.pineappl.lz4.tmp 2>/dev/null
-        rm ${i}/${dataset}.pineappl.lz4.tmp
-        mv ${i}/${dataset}.pineappl.lz4.tmp.lz4 ${grid}
+    if [[ ! -f ${metadata} ]]; then
+        echo "Skipping ${i#./}, couldn't find metadata (is the git repository on the right branch?)"
+        continue
+    else
+        echo "Updating ${i#./}"
     fi
+
+    eval $(awk -F= "BEGIN { printf \"pineappl set ${grid} ${grid}.tmp \" } { printf \"--entry %s '%s' \", \$1, \$2 }" ${metadata})
+
+    lz4 -9 ${i}.tmp 2>/dev/null
+    rm ${i}.tmp
+    mv ${i}.tmp.lz4 ${i}
 done

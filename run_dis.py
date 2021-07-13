f2423ae1730083500b001945dc4cb4060ed51cbd
Felix Hekhorn <felix.hekhorn@uni-tuebingen.de>
2021-07-13 18:04:20 +0200

    Add HERA observable runcards

---
+++ run_dis.py
@@ -0,0 +1,73 @@
+import yadism
+import yaml
+import pathlib
+import numpy as np
+import argparse
+
+here = pathlib.Path(__file__).parent
+
+runcards = here / "nnpdf31_proc"
+
+theory = {
+    "CKM": "0.97428 0.22530 0.003470 0.22520 0.97345 0.041000 0.00862 0.04030 0.999152",
+    "Comments": "LO baseline for small-x res",
+    "DAMP": 0,
+    "EScaleVar": 1,
+    "FNS": "ZM-VFNS",
+    "GF": 1.1663787e-05,
+    "HQ": "POLE",
+    "IC": 0,
+    "ID": 0,
+    "MP": 0.938,
+    "MW": 80.398,
+    "MZ": 91.1876,
+    "MaxNfAs": 6,
+    "MaxNfPdf": 6,
+    "ModEv": "EXA",
+    "NfFF": 3,
+    "PTO": 1,
+    "Q0": 1.0,
+    "QED": 0,
+    "Qedref": 1.777,
+    "Qmb": 4.5,
+    "Qmc": 2.0,
+    "Qmt": 173.07,
+    "Qref": 91.2,
+    "SIN2TW": 0.23126,
+    "SxOrd": "LL",
+    "SxRes": 0,
+    "TMC": 0,
+    "XIF": 1.0,
+    "XIR": 1.0,
+    "alphaqed": 0.007496251999999999,
+    "alphas": 0.11800000000000001,
+    "fact_to_ren_scale_ratio": 1.0,
+    "global_nx": 0,
+    "kDISbThr": 1.0,
+    "kDIScThr": 1.0,
+    "kDIStThr": 1.0,
+    "kbThr": 1.0,
+    "kcThr": 1.0,
+    "ktThr": 1.0,
+    "mb": 4.5,
+    "mc": 2.0,
+    "mt": 173.07,
+}
+
+
+def run_dataset(name):
+    with open(runcards / name / "observable.yaml") as o:
+        obs = yaml.safe_load(o)
+    out = yadism.run_yadism(theory, obs)
+    target = here / name
+    target.mkdir(exist_ok=True)
+    out.dump_pineappl_to_file(
+        str(target / f"{name}.pineappl"), next(iter(obs["observables"].keys()))
+    )
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("dataset")
+    args = ap.parse_args()
+    run_dataset(args.dataset)
---
+++ run_dis.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+prefix=$(pwd)/.prefix
+
+export LC_ALL=C
+
+install_pineappl() {(
+    mkdir -p "${prefix}"
+
+    cargo=$(which cargo 2> /dev/null || true)
+    git=$(which git 2> /dev/null)
+    pip=$(which pip 2> /dev/null || true)
+
+    repo=https://github.com/N3PDF/pineappl.git
+
+    if [[ ! -x ${cargo} ]]; then
+        export CARGO_HOME=${prefix}/cargo
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/rustup-init
+        bash /tmp/rustup-init --profile minimal --no-modify-path -y
+        export PATH=${prefix}/cargo/bin:${PATH}
+        cargo="${prefix}"/cargo/bin/cargo
+    elif [[ -d ${prefix}/cargo ]]; then
+        export CARGO_HOME=${prefix}/cargo
+    fi
+
+    if [[ -d ${prefix}/pineappl ]]; then
+        "${git}" pull
+    else
+        "${git}" clone ${repo} "${prefix}"/pineappl
+    fi
+
+    "${cargo}" install --force cargo-c
+
+    if [[ -x ${pip} ]]; then
+        pyver=$(python --version | cut -d' ' -f 2 | cut -d. -f1,2)
+        export PATH="${prefix}"/bin:${PATH}
+        export PYTHONPATH="${prefix}"/lib/python${pyver}/site-packages
+        "${pip}" install --prefix "${prefix}" maturin
+    fi
+
+    pushd . > /dev/null
+    cd "${prefix}"/pineappl/pineappl_py
+    maturin build --manylinux off
+    popd > /dev/null
+    "${pip}" install --prefix "${prefix}" "${prefix}"/pineappl/target/wheels/pineappl_py*.whl
+)}
+
+install_yadism() {
+    mkdir -p "${prefix}"
+
+    pip=$(which pip 2> /dev/null || true)
+
+
+    if [[ -x ${pip} ]]; then
+        pyver=$(python --version | cut -d' ' -f 2 | cut -d. -f1,2)
+        export PATH="${prefix}"/bin:${PATH}
+        export PYTHONPATH="${prefix}"/lib/python${pyver}/site-packages
+        "${pip}" install --prefix "${prefix}" yadism
+    fi
+
+}
+
+#install_pineappl
+install_yadism
+
+python run_dis.py "$@"

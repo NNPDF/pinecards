9a2a98d3fb2c8f3501e7553a1cf850d4659725c7
Alessandro Candido <candido.ale@gmail.com>
2021-10-01 19:22:35 +0200

    Revert original update script change

--- update_metadata.sh
+++ update_metadata.sh
@@ -10,22 +10,22 @@ set -o nounset
 set -o pipefail
 
 for i in $(find "$@" -name '*.pineappl.lz4'); do
-  dataset=$(basename $i)
-  dataset=${dataset%.pineappl.lz4}
+    dataset=$(basename $i)
+    dataset=${dataset%.pineappl.lz4}
 
-  export grid=${i}
-  export metadata=nnpdf31_proc/${dataset}/metadata.txt
+    export grid=${i}
+    export metadata=nnpdf31_proc/${dataset}/metadata.txt
 
-  if [[ ! -f ${metadata} ]]; then
-    echo "Skipping ${i#./}, couldn't find metadata (is the git repository on the right branch?)"
-    continue
-  else
-    echo "Updating ${i#./}"
-  fi
+    if [[ ! -f ${metadata} ]]; then
+        echo "Skipping ${i#./}, couldn't find metadata (is the git repository on the right branch?)"
+        continue
+    else
+        echo "Updating ${i#./}"
+    fi
 
-  eval $(awk -F= "BEGIN { printf \"pineappl set ${grid} ${grid}.tmp \" } { printf \"--entry %s '%s' \", \$1, \$2 }" ${metadata})
+    eval $(awk -F= "BEGIN { printf \"pineappl set ${grid} ${grid}.tmp \" } { printf \"--entry %s '%s' \", \$1, \$2 }" ${metadata})
 
-  lz4 -9 ${i}.tmp 2>/dev/null
-  rm ${i}.tmp
-  mv ${i}.tmp.lz4 ${i}
+    lz4 -9 ${i}.tmp 2>/dev/null
+    rm ${i}.tmp
+    mv ${i}.tmp.lz4 ${i}
 done

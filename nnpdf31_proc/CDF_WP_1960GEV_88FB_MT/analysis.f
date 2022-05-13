537a0488d64da33231e88e39b31ff395f8f33016
Christopher Schwan <handgranaten-herbert@posteo.de>
2022-05-13 14:44:58 +0200

    Add CDF transverse mass runcards

---
+++ nnpdf31_proc/CDF_WP_1960GEV_88FB_MT/analysis.f
@@ -0,0 +1,63 @@
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      subroutine analysis_begin(nwgt,weights_info)
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      implicit none
+      integer nwgt
+      character*(*) weights_info(*)
+
+      call HwU_inithist(nwgt,weights_info)
+      call HwU_book(1,'mt', 80, 60d0, 100d0)
+      return
+      end
+
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      subroutine analysis_end(dummy)
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      implicit none
+      double precision dummy
+      call HwU_write_file
+      return
+      end
+
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+      subroutine analysis_fill(p,istatus,ipdg,wgts,ibody)
+cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
+
+      implicit none
+      include 'nexternal.inc'
+      include 'cuts.inc'
+      integer istatus(nexternal)
+      integer iPDG(nexternal)
+      integer ibody
+      integer i
+      integer j
+      double precision p(0:4,nexternal)
+      double precision wgts(*)
+      double precision ppl(0:3), pplb(0:3), ppv(0:3), xmt, getabsy
+      logical is_nextph_iso(nexternal),is_nextph_iso_reco(nexternal)
+      external getabsy
+
+      double precision p_reco(0:4,nexternal)
+      integer iPDG_reco(nexternal)
+
+
+
+      call recombine_momenta(rphreco, etaphreco, lepphreco, quarkphreco,
+     $                       p, iPDG, is_nextph_iso, p_reco, iPDG_reco,
+     $                       is_nextph_iso_reco)
+
+      do j = nincoming+1, nexternal
+        if (iPDG_reco(j).eq.-13) pplb(0:3)=p_reco(0:3,j)
+        if (iPDG_reco(j).eq.14) ppl(0:3)=p_reco(0:3,j)
+      enddo
+      do i=0,3
+        ppv(i)=ppl(i)+pplb(i)
+      enddo
+
+      xmt=sqrt(2d0*sqrt((ppl(1)**2+ppl(2)**2)*(pplb(1)**2+pplb(2)**2))-
+     &         2d0*(ppl(1)*pplb(1)+ppl(2)*pplb(2)))
+
+      call HwU_fill(1,xmt,wgts)
+      end

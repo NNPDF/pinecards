89f3578e4e1c1c438882981b54adfff7e5389d84
juacrumar <juacrumar@lairen.eu>
2022-06-14 17:04:43 +0200

    define the vrap runcard with a vrap.yaml file

--- runcardsrunner/external/vrap.py
+++ runcardsrunner/external/vrap.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-vrap blablabla
+    Runner for vrap producing pineappl grids
+
+    Uses a modified version of pineappl https://github.com/NNPDF/hawaiian_vrap
+    which produces pineappl grids. It has been tested for FT DY kinematics
 """
 import subprocess as sp
+
+import numpy as np
+import pandas as pd
+import yaml
+from pineappl.grid import Grid
 
 from .. import install, paths
 from . import interface
@@ -14,7 +22,7 @@ def is_vrap(name):
     """
     Checks whether this is a dataset to be run with vrap
     """
-    return name.startswith("DYE")
+    return (paths.runcards / name / "vrap.yaml").exists()
 
 
 class Vrap(interface.External):
@@ -26,50 +34,56 @@ class Vrap(interface.External):
         kin_cards = list(self.source.glob(f"{self.name}*.dat"))
         if not kin_cards:
             raise FileNotFoundError(f"No kinematic cards found for {self.name}")
+        self._kin_cards = sorted(kin_cards)
 
-        order = "nlo"  # for now there are no other options
-        input_card = self.source / f"input{self.name}{order}.dat"
+        input_card = self.source / "vrap.yaml"
         if not input_card.exists():
             raise FileNotFoundError(
                 f"No vrap input card found for {self.name}: {input_card}"
             )
 
-        self._kin_cards = sorted(kin_cards)
-        self._input_card = input_card
+        # Load the run-specific options
+        input_yaml = yaml.safe_load(input_card.open("r", encoding="utf-8"))
+        input_yaml["PDFfile"] = f"{self.pdf}.LHgrid"
+
+        # Write the input card in the vrap format
+        self._input_card = (self.dest / self.name).with_suffix(".dat")
+        as_lines = [f"{k} {v}" for k, v in input_yaml.items()]
+        self._input_card.write_text("\n".join(as_lines))
         self._subgrids = []
 
     def run(self):
         """Run vrap for the given runcards"""
-        target = "deut" if "deut" in self._input_card.as_posix() else ""
         for b, kin_card in enumerate(self._kin_cards):
             sp.run(
                 [paths.vrap_exe, self._input_card, kin_card], cwd=self.dest, check=True
             )
-            pinename = f"{self.name}{target}_bin_{b}.pineappl.lz4"
-            sp.run(
-                ["pineappl", "optimize", _PINEAPPL, pinename], cwd=self.dest, check=True
-            )
-            self._subgrids.append(self.dest / pinename)
+            # Now optimize the grid
+            tmppine = self.dest / _PINEAPPL
+            grid = Grid.read(tmppine.as_posix())
+            grid.optimize()
+
+            # And give it a sensible name
+            if len(self._kin_cards) == 1:
+                pinename = self.grid
+            else:
+                pinename = self.source / f"{self.name}_bin_{b}.pineappl.lz4"
+
+            grid.write(pinename)
+            tmppine.unlink()
 
     def generate_pineappl(self):
-        """vrap generates all pineappl grid already at the ``run`` stage"""
-        # Remove possible intermediate pineappls
-        tmppine = self.dest / _PINEAPPL
-        if tmppine.exists():
-            tmppine.unlink()
-        # If we have one single grid, remove the bin index
-        if len(self._kin_cards) == 1:
-            self._subgrids[0].rename(self.grid)
+        pass
 
     def results(self):
-        import numpy as np
-        import pandas as pd
+        """Loads the results as reported by vrap in results.out"""
+        _, _, cv = np.loadtxt(self.dest / "results.out", unpack=True)
 
         d = {
-            "result": np.random.rand(2),
-            "error": np.random.rand(2),
-            "sv_min": np.random.rand(2),
-            "sv_max": np.random.rand(2),
+            "result": cv,
+            "error": np.zeros_like(cv),
+            "sv_min": np.zeros_like(cv),
+            "sv_max": np.zeros_like(cv),
         }
         return pd.DataFrame(data=d)
 

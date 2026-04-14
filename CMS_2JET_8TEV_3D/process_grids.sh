7ed82106c97a5ed909ad6076c6b006024b94fe80
Andrew Pietraszkiewicz <a.pietraszkiewicz@ed.ac.uk>
2026-04-14 17:21:16 +0100

    update CMS_2JET_8TEV_3D pinecard
    This change reflects the implementation of download_ploughshare feature in pinefarm

---
+++ CMS_2JET_8TEV_3D/process_grids.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+mkdir tmp
+
+# Bin mapping
+bin1="y_b=0.5, y*=0.5"
+bin2="y_b=0.5, y*=1.5"
+bin3="y_b=0.5, y*=2.5"
+bin4="y_b=1.5, y*=0.5"
+bin5="y_b=1.5, y*=1.5"
+bin6="y_b=2.5, y*=0.5"
+
+# Convert the grids to pineappl format under the right names
+for i in {0..5}; do
+    j=$((i+1))
+    src="$PS_DIR/${FILENAME}-xsec00${i}.tab.gz"
+    temp_dst="tmp/CMS_2JET_8TEV_3D_BIN${j}.pineappl.lz4"
+    dst="CMS_2JET_8TEV_3D_BIN${j}.pineappl.lz4"
+    pineappl import "$src" "$temp_dst" NNPDF40_nnlo_as_01180
+    eval "current_bin=\$bin${j}"
+    pineappl write --set-key-value kin_bin "${current_bin}" ${temp_dst} ${dst}
+    rm ${temp_dst}
+done
+
+rmdir tmp
+rm -r $FILENAME
+rm "${FILENAME}.tgz"

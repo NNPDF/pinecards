3cedaa169c2a33ea6270e7042b1bc944d8a1e284
Andrew Pietraszkiewicz <a.pietraszkiewicz@ed.ac.uk>
2026-04-28 10:39:39 +0200

    making CMS_2JET_8TEV_3D compatible with the current implementation of ploughshare download

--- CMS_2JET_8TEV_3D/postrun.sh
+++ CMS_2JET_8TEV_3D/postrun.sh
@@ -12,7 +12,7 @@ bin6="y_b=2.5, y*=0.5"
 # Convert the grids to pineappl format under the right names
 for i in {0..5}; do
     j=$((i+1))
-    src="$PS_DIR/${FILENAME}-xsec00${i}.tab.gz"
+    src="grid_${i}.tab.gz"
     temp_dst="tmp/CMS_2JET_8TEV_3D_BIN${j}.pineappl.lz4"
     dst="CMS_2JET_8TEV_3D_BIN${j}.pineappl.lz4"
     pineappl import "$src" "$temp_dst" NNPDF40_nnlo_as_01180
@@ -22,5 +22,4 @@ for i in {0..5}; do
 done
 
 rmdir tmp
-rm -r $FILENAME
-rm "${FILENAME}.tgz"
+rm "tmp.pineappl.lz4"
